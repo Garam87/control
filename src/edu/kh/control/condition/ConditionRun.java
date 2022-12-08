5aea97be65f4fac403f291be67a23516b0e09c90
garam <dheotjd87@gmail.com>
2022-12-08 14:16:18 +0900

    First control

---
+++ src/edu/kh/control/condition/ConditionRun.java
@@ -0,0 +1,17 @@
+package edu.kh.control.condition;
+
+public class ConditionRun {
+
+	public static void main(String[] args) {
+
+		ConditionExample ce = new ConditionExample();
+		
+//		ce.ex1();
+//		ce.ex2();
+//		ce.ex3();
+//		ce.ex4();
+//		ce.ex5();
+		ce.ex6();
+	}
+
+}
