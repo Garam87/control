da56df46ca34866a6646637b7e5a19658b45a77c
garam <dheotjd87@gmail.com>
2022-12-12 08:53:49 +0900

    third

---
+++ src/edu/kh/control/loop/LoopRun.java
@@ -0,0 +1,29 @@
+package edu.kh.control.loop;
+
+public class LoopRun {
+
+	public static void main(String[] args) {
+		ForExample fe = new ForExample();
+		
+//		fe.ex1();
+//		fe.ex2();
+//		fe.ex3();
+//		fe.ex4();
+//		fe.ex5();
+//		fe.ex6();
+//		fe.ex7();
+//		fe.ex10();
+//		fe.ex11();
+//		fe.ex12();
+//		fe.ex13();
+		
+		WhileExample we = new WhileExample();
+		
+	
+//		we.ex1();
+//		we.ex2();
+//		we.ex3();
+		we.ex4();
+	}
+
+}
